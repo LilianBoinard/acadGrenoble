2870fea7f5b3df9aa43f1d06e84f7f797a297c62
LilianB <72352117+LilianBoinard@users.noreply.github.com>
2020-10-08 17:30:35 +0200

    General Update
    Ajout d'un mode automatique, restructuration du code.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import requests
 import urllib.parse
+import sys
+from termcolor import colored, cprint
 
-def getID():
-    daten1 = int(input("Day: "))
-    daten2 = int(input("Month: "))
-    daten3 = int(input("Year: "))
+def process():
     params = {'daten1': daten1, 'daten2': daten2, 'daten3': daten3}
     params = urllib.parse.urlencode(params)
     r = requests.post('https://bv.ac-grenoble.fr/searchannu/src/infos_perso/etape2.php?login=%20')
@@ -12,13 +11,43 @@ def getID():
     txt = r.text
     a = "Informations" in txt
     if a:
-        test="lol"
         file = open("links.txt", "a")
         file.write(params)
         file.write('\n')
         file.close()
-        print("Utilisateur trouvé.")
-        getID()
+        resultText = daten1,daten2,daten3,': Utilisateur trouvé.'
+        cprint(resultText, 'green', 'on_grey')
     else:
-        print("Aucun utilisateur de ce nom.")
+        resultText = daten1,daten2,daten3,': Aucun utilisateur avec cette date.'
+        cprint(resultText, 'red', 'on_grey')
+
+def withInput():
+    global daten1
+    global daten2
+    global daten3
+    daten1 = int(input("Day: "))
+    daten2 = int(input("Month: "))
+    daten3 = int(input("Year: "))
+    
+def automatic():
+    global daten1
+    global daten2
+    global daten3
+    daten1 = 1
+    daten2 = 1
+    daten3 = 1900
+    while daten3 < 2000:
+        process()
+        daten3 += 1
+def getID():
+    choice = input("Mode pilote automatique ? (y/n)\n")
+    if choice == "y":
+        automatic()
+    elif choice == "n":
+        withInput()
+        process()
+    else:
+        print('Vous devez entrer "y" (oui) ou "n" (non)')
+        getID()
+    
 getID()
